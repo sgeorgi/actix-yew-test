bdeedd97b5c1922a5cce1aea5e8bec05da06d68a
Sebastian Georgi <sebastian@georgi.io>
2021-09-11 23:31:09 +0200

    Still not-compiling test for service handler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
@@ -11,4 +11,18 @@ async fn main() -> std::io::Result<()> {
         .bind("127.0.0.1:8080")?
         .run()
         .await
+}
+
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{http, test};
+
+    use super::*;
+
+    #[actix_rt::test]
+    async fn test_hello() {
+        let resp = hello().await;
+        assert_eq!(resp.status(), http::StatusCode::OK);
+    }
 }
