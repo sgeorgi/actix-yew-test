81d6300fd340040ecce89310fcf4e0242b25063f
Sebastian Georgi <sebastian@georgi.io>
2021-09-11 22:43:52 +0200

    Rustfmt

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,10 +7,7 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    })
+    HttpServer::new(|| App::new().service(hello))
         .bind("127.0.0.1:8080")?
         .run()
         .await
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -10,7 +10,10 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self { title: "This is message title".to_string(), body: "This is a message body".to_string() }
+        Self {
+            title: "This is message title".to_string(),
+            body: "This is a message body".to_string(),
+        }
     }
 
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
