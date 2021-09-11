9730f2c2260e953ea0a380a3e700f0b5d02aa58c
Sebastian Georgi <sebastian@georgi.io>
2021-09-11 22:57:07 +0200

    Scoped /api/v1 with hello

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,13 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
+#[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(web::scope("/api/v1").service(hello)))
         .bind("127.0.0.1:8080")?
         .run()
         .await
