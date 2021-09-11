78a20517095b4e4f28a023a399e417a9bb6b95ab
Sebastian Georgi <sebastian@georgi.io>
2021-09-11 16:21:44 +0200

    Created a small Yew component

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,36 @@
+use yew::prelude::*;
+
+struct Model {
+    title: String,
+    body: String,
+}
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self { title: "This is message title".to_string(), body: "This is a message body".to_string() }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <p>{ &self.title }</p>
+                <p>{ &self.body }</p>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>()
 }
